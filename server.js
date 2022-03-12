c2bc42fe9d63d499ade9848f5f8030ffc512f6af
Cherrypick14 <cherrypicklet@gmail.com>
2022-03-13 01:36:36 +0300

    added the user route

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv")
 const PORT = process.env.PORT || 5000
 const connectedDB = require("./database/connect");
 const postrouter = require("./routes/post");
+const user_router=require("./routes/user");
 
 const app = express();
  dotenv.config();
@@ -15,7 +16,7 @@ connectedDB(process.env.MONGO_URL)
  app.use(express.urlencoded({extended:true}))
 //endpoints
 app.use('/api/v1/post', postrouter)
-
+app.use('/api/v1/user', user_router)
 app.listen(PORT, ()=>{
     console.log(`server is listening at ${PORT}`);
 })
